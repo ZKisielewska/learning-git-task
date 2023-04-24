ba3ac5f2e4823f3122a1e12609e5780559522bae
ZKisielewska <kisielewskazpl@gmail.com>
2023-04-24 17:56:11 +0200

    Modify shopping_list.py

--- shopping_list.py
+++ shopping_list.py
@@ -3,3 +3,8 @@ shopping_list = {
     "iSpot": ["laptop", "keyboard", "mouse"],
     "Empik": ["books", "notebooks", "pencils"]
 }
+
+# using for loop we iterate through the dictionary
+# store and products names should be capitalized
+for store, product in shopping_list.items():
+    print(f"I'll go to {store.title()} and buy there following products: {str(product).title()}.")
