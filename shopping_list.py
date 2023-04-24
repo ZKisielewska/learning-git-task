1c40b66681b0125c8bbad786795e10202408bf32
ZKisielewska <kisielewskazpl@gmail.com>
2023-04-24 18:19:41 +0200

    Add f-strings format and list comprehension

--- shopping_list.py
+++ shopping_list.py
@@ -8,3 +8,7 @@ shopping_list = {
 # store and products names should be capitalized
 for store, product in shopping_list.items():
     print(f"I'll go to {store.title()} and buy there following products: {str(product).title()}.")
+
+# using list comprehension we calculate the amount of purchased products
+amount_products = sum([len(product) for product in shopping_list.values()])
+print(f"I'll buy {amount_products} products in total.")
